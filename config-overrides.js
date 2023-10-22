2432c144abdb4301d4117e045dcf9a683ac14ab5
Aditya Vikram <adityavikram2016@gmail.com>
2023-10-21 23:46:22 -0400

    removed

--- config-overrides.js
+++
@@ -1,7 +0,0 @@
-// config-overrides.js - override the webpack config
-module.exports = function override(config, env) {
-    // Change the target to electron-renderer
-    config.target = "electron-renderer";
-    return config;
-  };
-  
